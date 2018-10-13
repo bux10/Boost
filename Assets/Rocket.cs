57dffb9ccafa86cd13ea9661f301a644858b703a
Dustin Moore <dmoore@frostbytecafe.com>
2018-10-13 10:53:30 -0600

    Added thrust and rotate behaviour to rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
